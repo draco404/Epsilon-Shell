e55062ef700c80f676efea71a260fccb171fdd50
Alejandro Vieyra <alejandrovieyra7@gmail.com>
2022-10-08 18:06:29 -0500

    Start of Epsilon Shell

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::{io::{stdin, stdout, Write}, process::Command};
+
+fn main() {
+    loop {
+        print!(">");
+        stdout().flush().unwrap();
+    
+        let mut input = String::new();
+        stdin()
+            .read_line(&mut input)
+            .expect("Error on Input");
+
+        let command = input.trim();
+
+        let mut child = Command::new(command).spawn().unwrap();
+        child.wait().unwrap();
+    }
+}
+
